3b8f8703bbaba3a1e05b01b299adef953bed73e3
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 23:01:26 +0700

    A class is also an object in Python

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from person import Person
 if __name__ == "__main__":
   # Define a class
 
-  person = Person()
+  # person = Person()
 
-  print(person)
-  print(type(person))
-  print(id(person))
-  print(hex(id(person)))
-  print(isinstance(person, Person))
+  # print(person)
+  # print(type(person))
+  # print(id(person))
+  # print(hex(id(person)))
+  # print(isinstance(person, Person))
+
+  # A class is also an object in Python
+
+  print(Person.__name__)
+  print(type(Person))
