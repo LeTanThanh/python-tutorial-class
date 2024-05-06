530fecd152f6ed343cbd9ac9fb96d38dddc18fcd
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 23:00:25 +0700

    Define a class

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from person import Person
+
 if __name__ == "__main__":
-  print("Python Class")
+  # Define a class
+
+  person = Person()
+
+  print(person)
+  print(type(person))
+  print(id(person))
+  print(hex(id(person)))
+  print(isinstance(person, Person))
